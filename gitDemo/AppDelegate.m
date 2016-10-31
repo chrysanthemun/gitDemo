c465fe674f098fedaa08aac61ffbabc1a20f8f43
juhua xie <juhuaxie@juhuadeMacBook-Pro.local>
2016-10-31 21:49:47 +0800

    练习git操作

--- gitDemo/AppDelegate.m
+++ gitDemo/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,6 +16,10 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    ViewController *view = [[ViewController alloc] init];
+    self.window.rootViewController = view;
+    [self.window makeKeyAndVisible];
     // Override point for customization after application launch.
     return YES;
 }
