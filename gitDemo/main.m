20f49d39825903f4c222be1da88be23f8575f9c7
juhua xie <juhuaxie@juhuadeMacBook-Pro.local>
2016-10-31 21:24:40 +0800

    Initial Commit

---
+++ gitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitDemo
+//
+//  Created by juhua xie on 2016/10/31.
+//  Copyright © 2016年 juhua xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
