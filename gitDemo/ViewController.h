2b46f349cc4a09d907304bcfc184caa6f88b6f4a
juhua xie <juhuaxie@juhuadeMacBook-Pro.local>
2016-11-07 21:16:34 +0800

    添加项目进仓库

---
+++ gitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by juhua xie on 2016/10/31.
+//  Copyright © 2016年 juhua xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitDemo/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  gitDemo
+//
+//  Created by juhua xie on 2016/10/31.
+//  Copyright © 2016年 juhua xie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIView *view = [[UIView alloc] init];
+    /////hsdhvdav
+    [self.view addSubview:view];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
